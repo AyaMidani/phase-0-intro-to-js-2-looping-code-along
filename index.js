1213802203c7a0752b1b9bc15d6550ceda85572b
AyaMidani <61182291+AyaMidani@users.noreply.github.com>
2022-04-17 02:48:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+function writeCards(Array,surprise) {
+    const newArray=[];
+  for (let i = 0; i < Array.length; i++) {
+    newArray.push(`Thank you, ${Array[i]}, for the wonderful ${surprise} gift!`);
+  }
+  return newArray;
+}
+function countDown(number)
+{
+    while(number>=0)
+    {
+        console.log(number)
+        number--;
+    }
+}
+
+writeCards(["Guadalupe","Ollie","Aki"], "surprise");
+countDown(4)
